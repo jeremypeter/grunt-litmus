817b0f5cf26e1a6ef623b47849367e106c1f3956
Jeremy Peter <jeremy.peter@meltmedia.com>
2014-01-22 12:23:00 -0700

    Taking caring of duplicate titles

--- tasks/litmus.js
+++ tasks/litmus.js
@@ -21,7 +21,8 @@ module.exports = function(grunt) {
     var options = this.options(),
         done = this.async(),
         date = grunt.template.today('yyyy-mm-dd'),
-        files = this.filesSrc;
+        files = this.filesSrc,
+        titleDups = {};
 
     // Iterate over all specified file groups.
     async.eachSeries(files, function(file, next) {
@@ -31,7 +32,7 @@ module.exports = function(grunt) {
           subject = options.subject,
           $       = cheerio.load(html),
           $title  = $('title').text().trim();
-
+    
       if( (subject === undefined) || (subject.trim().length === 0) ){
         subject = $title;
       }
@@ -39,8 +40,23 @@ module.exports = function(grunt) {
       // If no subject or title then set to date
       if(subject.trim().length === 0){
         subject = date;
-      }  
-      
+      }
+
+      // Add +1 if duplicate titles exists
+      if(files.length > 1){
+
+        if(titleDups[subject] === undefined){
+          titleDups[subject] = [];
+        }else{
+          titleDups[subject].push(subject);
+        }
+        
+        if(titleDups[subject].length){
+          subject = subject + ' - ' + parseInt(titleDups[subject].length + 1, 10);
+        }
+
+      }
+
       litmus.run(html, subject.trim(), next);
 
     }, function() {

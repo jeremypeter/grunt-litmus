41b6f1c9b399d7b23a71b16b05173b16f9309ef9
Jeremy Peter <jeremy.peter@meltmedia.com>
2014-05-01 23:31:08 -0700

    adjusting test

--- Gruntfile.js
+++ Gruntfile.js
@@ -104,7 +104,8 @@ module.exports = function(grunt) {
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'litmus', 'nodeunit']);
+  grunt.registerTask('test', ['nodeunit']);
+  grunt.registerTask('litmus', ['clean', 'litmus', 'nodeunit']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);

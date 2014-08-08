85b4621e77c60c3195540089bbe6e7cbdcfdba82
Jeremy Peter <jeremy.peter@meltmedia.com>
2014-08-08 10:14:59 -0700

    renaming task because it conflicts with config name

--- Gruntfile.js
+++ Gruntfile.js
@@ -105,7 +105,7 @@ module.exports = function(grunt) {
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
   grunt.registerTask('test', ['nodeunit']);
-  grunt.registerTask('litmus', ['clean', 'litmus', 'nodeunit']);
+  grunt.registerTask('runLitmus', ['clean', 'litmus', 'nodeunit']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
